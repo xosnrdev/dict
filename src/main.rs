2c41a10fdb3d86ffe5d5ccb7171862f82de4eee0
xosnrdev <successxodev@gmail.com>
2024-09-22 22:46:57 -0400

    fetch word definition and read response

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 const API_ENDPOINT: &str = "https://api.dictionaryapi.dev/api/v2/entries/en_US";
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let word = std::env::args().nth(1);
 
     if let Some(word) = word {
-        println!("{}", resource_uri(word))
+        println!("{}", get_word_definition(word).await)
     }
 }
 
 fn resource_uri(word: String) -> String {
     format!("{}/{}", API_ENDPOINT, word)
+}
+
+async fn get_word_definition(word: String) -> String {
+    let url = resource_uri(word);
+    let response = reqwest::get(url).await.expect("Could not fetch resource.");
+
+    response.text().await.expect("Could not read the response.")
 }
