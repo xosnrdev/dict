0c9dc692d9940526d8a162c1a47b1e1f656d7a3c
xosnrdev <successxodev@gmail.com>
2024-09-22 23:43:57 -0400

    not a fan of expect

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use anyhow::{Context, Result};
+
 const API_ENDPOINT: &str = "https://api.dictionaryapi.dev/api/v2/entries/en_US";
 
 #[derive(serde::Deserialize, Debug)]
@@ -16,31 +18,33 @@ struct Definition {
 }
 
 #[tokio::main]
-async fn main() {
-    let word = std::env::args().nth(1);
+async fn main() -> Result<()> {
+    let word = std::env::args().nth(1).context("Please enter a word.")?;
 
-    if let Some(word) = word {
-        let definition = get_word_definition(word).await;
-        for meaning in definition.meanings {
-            for definition in meaning.definitions {
-                println!("{}", definition.definition)
-            }
+    let definition = get_word_definition(word).await?;
+    for meaning in definition.meanings {
+        for definition in meaning.definitions {
+            println!("{}", definition.definition)
         }
     }
+
+    Ok(())
 }
 
 fn format_resource_uri(word: String) -> String {
     format!("{}/{}", API_ENDPOINT, word)
 }
 
-async fn get_word_definition(word: String) -> ParsedResponse {
+async fn get_word_definition(word: String) -> Result<ParsedResponse> {
     let url = format_resource_uri(word);
-    let response = reqwest::get(url).await.expect("Could not fetch resource.");
+    let response = reqwest::get(url).await?;
 
-    let parsed_response: Vec<ParsedResponse> =
-        response.json().await.expect("Could not read the response.");
+    let parsed_response: Vec<ParsedResponse> = response
+        .json()
+        .await
+        .context("Please enter a valid word.")?;
     parsed_response
         .into_iter()
         .nth(0)
-        .expect("Could not find first element.")
+        .context("Could not find first element.")
 }
