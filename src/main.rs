1a1dfad15cef658892fce314643af75933a1f717
xosnrdev <successxodev@gmail.com>
2024-09-22 22:32:04 -0400

    Build dictionary resource endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+const API_ENDPOINT: &str = "https://api.dictionaryapi.dev/api/v2/entries/en_US";
+
 fn main() {
     let word = std::env::args().nth(1);
 
     if let Some(word) = word {
-        println!("{}", word)
+        println!("{}", resource_uri(word))
     }
+}
+
+fn resource_uri(word: String) -> String {
+    format!("{}/{}", API_ENDPOINT, word)
 }
