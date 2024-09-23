e3b6ecb34c2af164e89bd8547fe5b94ae34ed6bb
xosnrdev <successxodev@gmail.com>
2024-09-22 23:07:26 -0400

    parse response into required json constraint

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 const API_ENDPOINT: &str = "https://api.dictionaryapi.dev/api/v2/entries/en_US";
 
+#[derive(serde::Deserialize, Debug)]
+struct ParsedResponse {
+    meanings: Vec<Meanings>,
+}
+
+#[derive(serde::Deserialize, Debug)]
+struct Meanings {
+    definitions: Vec<Definition>,
+}
+
+#[derive(serde::Deserialize, Debug)]
+struct Definition {
+    definition: String,
+}
+
 #[tokio::main]
 async fn main() {
     let word = std::env::args().nth(1);
 
     if let Some(word) = word {
-        println!("{}", get_word_definition(word).await)
+        println!("{:?}", get_word_definition(word).await)
     }
 }
 
@@ -13,9 +28,14 @@ fn resource_uri(word: String) -> String {
     format!("{}/{}", API_ENDPOINT, word)
 }
 
-async fn get_word_definition(word: String) -> String {
+async fn get_word_definition(word: String) -> ParsedResponse {
     let url = resource_uri(word);
     let response = reqwest::get(url).await.expect("Could not fetch resource.");
 
-    response.text().await.expect("Could not read the response.")
+    let parsed_response: Vec<ParsedResponse> =
+        response.json().await.expect("Could not read the response.");
+    parsed_response
+        .into_iter()
+        .nth(0)
+        .expect("Could not find first element.")
 }
